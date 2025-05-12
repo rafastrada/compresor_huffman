d793b749c839d2af010e0d72226261a63d58c693
rafastrada <guillerafael98@gmail.com>
2025-05-12 19:13:03 -0300

    Readme nuevo y cambios en CLI
    - Se creó el archivo README.
    - El ejecutable (CLI) ahora muestra la ayuda cuando se ejecuta sin
    parámetros.

--- Cli_huffman.c
+++ Cli_huffman.c
@@ -1,4 +1,5 @@
 #include "Huffman.h"
+#include <stdio.h>
 #include <string.h>
 
 
@@ -7,9 +8,21 @@ int main(int argc, char *argv[])
 	if (argc == 3) {
 		char *nombre_archivo = argv[2];
 
-		if (strcmp(argv[1], "com") == 0)
+		if (strcmp(argv[1], "comprimir") == 0)
 			ArchivoCodificar(nombre_archivo);
-		else ArchivoDecodificar(nombre_archivo);
+		else {
+			if (strcmp(argv[1], "descomprimir"))
+				ArchivoDecodificar(nombre_archivo);
+		}
+	} else {
+		printf(
+				"\nCompresor de archivos por codificación de Huffman\n\n"
+				"Uso: huffman.exe (operacion) (nombre de archivo)\n"
+				"Operaciones disponibles:\n"
+				"\t'comprimir' : Comprime un archivo en uno nuevo con extensión \".huf\".\n"
+				"\t'descomprimir' : Descomprime un archivo de extensión \".huf\", obteniendo el archivo original."
+				"El resultado posee extensión \".dhu\".\n"
+			  )
 	}
 
 	return 0;
---
+++ README.md
@@ -0,0 +1,15 @@
+# Compresor Huffman
+
+Para compilar el programa debe usarse el siguiente comando (si se usa GCC):
+
+```
+gcc -Wall -g3 Cli_huffman.c Huffman.c -o huffman.exe
+
+```
+
+`-o <path/nombre_ejecutable>` es la dirección y nombre del ejecutable del programa.
+Puede ser en cualquier lugar.
+
+`-Wall` muestra todas las advertencias del compilador.
+
+`-g3` es necesario incluir solo por propósitos de depuración.
